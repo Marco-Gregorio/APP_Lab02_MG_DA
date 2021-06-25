a8d64c26a33e3faac2ef671bd3b2288b3ee966c8
Marco-Gregorio <you@example.com>
2021-06-25 06:32:29 -0400

    segundo commit

--- src/app/paginas/registro-formulario/registro-formulario.page.ts
+++ src/app/paginas/registro-formulario/registro-formulario.page.ts
@@ -1,5 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+//importar servicio
+import { FormularioService } from '../../servicios/formulario.service'
+
+export class EnviarDatos{
+  constructor(
+    public dato1: string,
+    public dato2: string,
+    public dato3: string,
+    public dato4: string,
+  ){}
+}
+
 @Component({
   selector: 'app-registro-formulario',
   templateUrl: './registro-formulario.page.html',
@@ -7,9 +19,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroFormularioPage implements OnInit {
 
-  constructor() { }
+  datos_formulario;
+
+  constructor(private formularioService: FormularioService) {
+    this.datos_formulario = new EnviarDatos("dato 1", "dato 2", "dato 3", "dato 4");
+  }
 
   ngOnInit() {
   }
+
+  enviarDatos() {
+    this.formularioService.registrarFormulario(this.datos_formulario).subscribe(
+      (response:any)=>{
+        if(response.registro){
+          alert("datos registrados correctamente")
+        }else{
+          alert("Datos NO registrados")
+        }
+      },
+      error=>{
+        alert("error en registrar datos")
+      }
+    )
+    console.log("Datos del formulario: ", this.datos_formulario);
+  }
+
+//  obtenerFormulario(){
+//    return this.http.get(`${this.backend}/obtener-registros`); }
 
 }
